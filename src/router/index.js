a0d695fd51825061d25596125232e238ec51a71d
itsjamesmurray <james@itsjamesmurray.com>
2020-01-02 21:48:37 -0600

    adding vue-meta tags.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Meta from 'vue-meta'
+import Meta from 'vue-meta'
 
 Vue.use(VueRouter)
-// Vue.use(Meta)
+Vue.use(Meta)
 
 const routes = [
   {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -74,17 +74,6 @@ export default {
     BlogRoll,
     Loading
    },
-  // metaInfo() {
-  //   return {
-  //     title: 'I am a visionary! | ItsJamesMurray',
-  //     meta: [
-  //       {
-  //         name: 'description',
-  //         content: 'Welcome to ItsJamesMurray!  I created this site as a place to fail in public. I know I want to build something great—I\'m just not quite sure I\'ve found out what yet. I\'ll be using this little corner of the internet to document my journey.'
-  //       }
-  //     ]
-  //   }
-  // },
    methods: {
      navToBlog (){
        this.$router.push({name: 'blog'})
